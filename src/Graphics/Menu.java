3c63724b83af1e6f2836b854af473be1b3dbfdcb
Michał Bernacki-Janson <94766391+Trylog@users.noreply.github.com>
2022-10-08 18:12:09 +0200

    start

---
+++ src/Graphics/Menu.java
@@ -0,0 +1,31 @@
+package Graphics;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Menu extends JFrame implements ActionListener {
+
+	JButton button1, button2, button3;
+
+	public Menu(){
+
+		this.setTitle("Symulogic");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setLocationRelativeTo(null);
+		this.setLayout(null);
+		this.setSize(400, 500);
+		this.setVisible(true);
+
+
+	}
+
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
